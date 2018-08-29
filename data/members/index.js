41223f6ad3df504fc6ad050e9712ebc7b41fb0e3
Somesh Kar <you@ip-172-31-29-228.ap-south-1.compute.internal>
2018-08-29 08:01:42 +0000

    Updated members and faculty
    Committer: Somesh Kar <i@somesh.co>
    	modified:   data/faculty/index.js
    	modified:   data/members/index.js
    	new file:   package-lock.json

--- data/members/index.js
+++ data/members/index.js
@@ -153,6 +153,16 @@ export default [
     year: '9th',
     members: [
       {
+        name: 'Angad Singh',
+        role: 'Member',
+        image: '',
+      },
+      {
+        name: 'Ashvin Verma',
+        role: 'Member',
+        image: '',
+      },
+      {
         name: 'Somesh Kar',
         role: 'Member',
         image: '',
@@ -178,20 +188,30 @@ export default [
         image: '',
       },
       {
-        name: 'Angad Singh',
-        role: 'Member',
-        image: '',
-      },
-      {
-        name: 'Ashvin Verma',
-        role: 'Member',
-        image: '',
-      },
-      {
         name: 'Saathvik Sharma',
         role: 'Member',
         image: '',
       },
+      {
+        name: 'Pulkit Garg',
+        role: 'Member',
+        image: '',
+      },
+      {
+        name: 'Sarthak Mangla',
+        role: 'Member',
+        image: '',
+      },
+      {
+        name: 'Shashwat Mundra',
+        role: 'Member',
+        image: '',
+      },
+      {
+        name: 'Aryaman Chandra',
+        role: 'Member',
+        image: ''
+      }
     ],
   },
   {
