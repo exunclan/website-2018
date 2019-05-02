fc7411c780e5e6f5efde25ac0160094ac91cbf6d
Kabir Goel <kabirgoel.kg@gmail.com>
2019-05-03 04:58:53 +0530

    Add plugins to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
+const path = require(`path`)
+
 module.exports = {
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: path.join(__dirname, 'src', 'images'),
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ],
 }
